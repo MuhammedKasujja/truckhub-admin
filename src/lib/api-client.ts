8f91b3a7dc502687d809071453781c9231568732
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-03-02 11:44:33 +0300

    feat: add auth token to endpoints

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,10 +1,26 @@
 import axios from "axios";
+import { getAccessToken } from "./session";
 
-const apiClient = axios.create({ baseURL: `${process.env.BACKEND_URL}` });
+const apiClient = axios.create({
+  baseURL: `${process.env.BACKEND_URL}`,
+  timeout: 15000, // ← prevents hanging forever after 15 secs
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
 apiClient.interceptors.request.use(
-  (request) => {
-    return request;
+  async (config) => {
+    // Skip adding token to login / refresh endpoints
+    // const noAuthEndpoints = ["/login", "/refresh-token"];
+
+    if (config.url?.includes("login")) {
+      return config;
+    }
+    const accessToken = await getAccessToken();
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
   },
   (error) => {
     // console.error("Error.....", error);
@@ -13,17 +29,19 @@ apiClient.interceptors.request.use(
 );
 
 apiClient.interceptors.response.use(
-  (response) => {
-    return Promise.resolve(response);
-  },
+  (response) => response,
   (error) => {
     console.error("ApiError.....", error);
-    if (error.code === "ECONNREFUSED") {
+    if (
+      error.code === "ECONNREFUSED" ||
+      error.message.includes("Network Error")
+    ) {
       error.response = {
         data: {
+          status: 503,
           success: false,
           error: {
-            code: "ECONNREFUSED",
+            code: "SERVICE_UNAVAILABLE",
             message: "Could not connect to server",
           },
         },
