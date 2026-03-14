5b7b3ea8009cc87aaeb56c57c8ddce98be509390
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-03-15 01:46:16 +0300

    feat: view booking details page

---
+++ src/app/(admin)/bookings/[id]/view/page.tsx
@@ -0,0 +1,15 @@
+import { BookingDetailsWrapper } from "@/features/bookings/components/booking-details-wrapper";
+import { getBookingDetailsById } from "@/features/bookings/service";
+import { Suspense } from "react";
+
+export default async function Page(props: PageProps<"/users/[id]/view">) {
+  const promises = Promise.all([
+    getBookingDetailsById((await props.params).id),
+  ]);
+
+  return (
+    <Suspense fallback={<div>Loading booking details...</div>}>
+      <BookingDetailsWrapper promises={promises} />
+    </Suspense>
+  );
+}
---
+++ src/features/bookings/components/booking-details-wrapper.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useFetchEror } from "@/hooks/use-fetch-error";
+import { Edit2Icon } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import { getBookingDetailsById } from "@/features/bookings/service";
+import { formatDate } from "@/lib/format";
+import { Status } from "@/components/ui/status";
+
+type BookingDetailsWrapperProps = {
+  promises: Promise<[Awaited<ReturnType<typeof getBookingDetailsById>>]>;
+};
+
+export function BookingDetailsWrapper({
+  promises,
+}: BookingDetailsWrapperProps) {
+  const [{ data: booking, error }] = React.use(promises);
+
+  useFetchEror(error);
+
+  return (
+    <div className="grid grid-cols-5 grid-flow-col gap-5">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            {booking?.origin} - {booking?.destination}
+          </CardTitle>
+          <CardAction>
+            <Status>{booking?.status}</Status>
+            <Button asChild>
+              {/* <Link href={`/bookings/${booking}/edit`}> */}
+              <Edit2Icon />
+              {/* </Link> */}
+            </Button>
+          </CardAction>
+          <CardDescription></CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div>{booking?.customer.email}</div>
+          <div>{booking?.customer.phone}</div>
+          <div>{formatDate(booking?.created_at)}</div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
