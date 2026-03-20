2dfbee4a958b48f11afdae846f01322f8553e706
MuhammedKasujja <al.kasmud.2@gmail.com>
2026-03-20 13:52:00 +0300

    feat: proxy.ts remove next static generated routes

--- src/proxy.ts
+++ src/proxy.ts
@@ -10,3 +10,7 @@ export async function proxy(request: NextRequest) {
   }
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|api/).*)"],
+};
